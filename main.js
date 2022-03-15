934afd68b59b832d378877e87f7c5cbfb3566b05
Jean Probst <45835269+jeanprobst@users.noreply.github.com>
2022-03-15 20:35:19 -0300

    Adding and adjusting rules
    - Inches and feet calculation for distance category
    - Disabled other kinds of measurements

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+// IDEAS TO IMPROVE THE CODE FOR THE FUTURE
+
+/* wondering in a math relation among the category(kind). Probably, something like a matrix
+of relational number. In this case, it would be separated by category(kind)...
+for example, distance, space, speed, weight etc.
+
+// DISTANCE
+
+var meter = 1;
+var centimeter = 100;
+var kilometer = 1000;
+
+// meter
+var nova = 1200;
+
+// kilometer
+var nova1 = nova / kilometer
+
+console.log(nova1)
+
+*/
+
+
 var kind = document.getElementById("kind").value;
+
 
 function distance() {
     if (kind == "distance") {
@@ -82,4 +106,157 @@ function getValue()
     }
 
 return chosen;
+}
+
+function myCalcInches(a) {
+    const distance = document.getElementById("distance").value;
+    if(distance == "milimeter") {
+
+    const calc = a / 25.4
+
+    console.log(calc.toFixed(2))
+
+    document.getElementById("outcome-value").value = calc.toFixed(2);
+    }
+    else if(distance == "centimeter") {
+
+        const calc = a / 2.54
+    
+        console.log(calc.toFixed(2))
+    
+        document.getElementById("outcome-value").value = calc.toFixed(2);
+        }
+        else if(distance == "meter") {
+
+            const calc = a / 0.0254
+        
+            console.log(calc.toFixed(2))
+        
+            document.getElementById("outcome-value").value = calc.toFixed(2);
+            }
+            else if(distance == "kilometer") {
+
+                const calc = a / 0.0000254
+            
+                console.log(calc.toFixed(2))
+            
+                document.getElementById("outcome-value").value = calc.toFixed(2);
+                }
+                else if(distance == "miles") {
+
+                    const calc = a / 0.0000157828
+                
+                    console.log(calc.toFixed(2));
+                
+                    document.getElementById("outcome-value").value = calc.toFixed(0);
+                    }
+                    else if(distance == "inches") {
+
+                        const calc = a
+                    
+                        console.log(calc.toFixed(2));
+                    
+                        document.getElementById("outcome-value").value = calc.toFixed(2);
+                        }
+                        else if(distance == "feet") {
+
+                            const calc = a * 12
+                        
+                            console.log(calc.toFixed(2));
+                        
+                            document.getElementById("outcome-value").value = calc.toFixed(2);
+                            }
+                            else if(distance == "yards") {
+
+                                const calc = a * 36
+                            
+                                console.log(calc.toFixed(2));
+                            
+                                document.getElementById("outcome-value").value = calc.toFixed(2);
+                                }
+                        
+}
+
+function myCalcFeet(a) {
+    const distance = document.getElementById("distance").value;
+    if(distance == "milimeter") {
+
+    const calc = a / 304.8
+
+    console.log(calc.toFixed(5))
+
+    document.getElementById("outcome-value").value = calc.toFixed(5);
+    }
+    else if(distance == "centimeter") {
+
+        const calc = a / 30.48
+    
+        console.log(calc.toFixed(2))
+    
+        document.getElementById("outcome-value").value = calc.toFixed(5);
+        }
+        else if(distance == "meter") {
+
+            const calc = a * 3.281
+        
+            console.log(calc.toFixed(2))
+        
+            document.getElementById("outcome-value").value = calc.toFixed(3);
+            }
+            else if(distance == "kilometer") {
+
+                const calc = a * 3281
+            
+                console.log(calc.toFixed(2))
+            
+                document.getElementById("outcome-value").value = calc.toFixed(2);
+                }
+                else if(distance == "miles") {
+
+                    const calc = a * 5280
+                
+                    console.log(calc.toFixed(2));
+                
+                    document.getElementById("outcome-value").value = calc.toFixed(0);
+                    }
+                    else if(distance == "inches") {
+
+                        const calc = a / 12
+                    
+                        console.log(calc.toFixed(2));
+                    
+                        document.getElementById("outcome-value").value = calc.toFixed(2);
+                        }
+                        else if(distance == "feet") {
+
+                            const calc = a
+                        
+                            console.log(calc.toFixed(2));
+                        
+                            document.getElementById("outcome-value").value = calc.toFixed(2);
+                            }
+                            else if(distance == "yards") {
+
+                                const calc = a * 3
+                            
+                                console.log(calc.toFixed(2));
+                            
+                                document.getElementById("outcome-value").value = calc.toFixed(2);
+                                }
+                        
+}
+
+/*
+
+*/
+
+function check() {
+    var e = document.getElementById("distance-out").value;
+
+    if(e === "inches") {
+        myCalcInches(document.getElementById("entry-value").value);
+    }
+    else if(e === "feet") {
+        myCalcFeet(document.getElementById("entry-value").value);
+    }
 }
